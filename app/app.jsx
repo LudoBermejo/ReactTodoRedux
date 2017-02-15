01427769687f3ad4b2f479d7ef6191babcf4d4bb
Ludo Bermejo <ludobermejo@gmail.com>
2017-02-15 15:05:14 +0100

    Just before going mad with redux

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+
 import TodoApp from 'TodoApp';
+import store from 'reduxStore';
+
 
 // Load foundation
 $(document).foundation();
@@ -9,19 +13,9 @@ $(document).foundation();
 // App CSS
 import 'AppStyles';
 
-import * as reduxActions from 'actions';
-import store from 'reduxStore';
-
-store.subscribe(() => {
-  console.log('State', store.getState())
-})
-
-store.dispatch(reduxActions.addTodo('pepe'))
-store.dispatch(reduxActions.setTextSearch('p'))
-store.dispatch(reduxActions.toggleShowCompleted())
-
-console.log(store);
 ReactDOM.render(
-  <TodoApp />,
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('app')
 );
